ddbbaa482e15d52bed1d5c592ee0139653404adc
jrinta- <jrinta-@c2r3p12.hive.fi>
2024-06-27 20:38:27 +0300

    working on c01

--- c01/ft_ultimate_div_mod.c
+++ c01/ft_ultimate_div_mod.c
@@ -1,19 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_div_mod.c                                       :+:      :+:    :+:   */
+/*   ft_ultimate_div_mod.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jrinta- <jrinta-@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/27 20:04:51 by jrinta-           #+#    #+#             */
-/*   Updated: 2024/06/27 20:22:00 by jrinta-          ###   ########.fr       */
+/*   Created: 2024/06/27 20:23:20 by jrinta-           #+#    #+#             */
+/*   Updated: 2024/06/27 20:29:43 by jrinta-          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_div_mod(int a, int b, int *div, int *mod)
+void	ft_ultimate_div_mod(int *a, int *b)
 {
-	*div = a / b;
-	*mod = a % b;
+	int c;
+	int d;
+
+	c = *a / *b;
+	d = *a % *b;
+	*a = c;
+	*a = d;
+}
+
+void	print_char(char c)
+{
+	write(1, &c, 1);
+}
+
+void	main(void)
+{
+	int a;
+	int b;
+	
+	a = 17;
+	b = 5;
+
+	ft_ultimate_div_mod(&a, &b);
+	print_char(a + '0');
+	print_char(b + '0');
 }
--- c01main/ex03/ft_div_mod.c
+++ c01main/ex03/ft_div_mod.c
@@ -6,7 +6,7 @@
 /*   By: jrinta- <jrinta-@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 20:04:51 by jrinta-           #+#    #+#             */
-/*   Updated: 2024/06/27 20:22:00 by jrinta-          ###   ########.fr       */
+/*   Updated: 2024/06/27 20:20:09 by jrinta-          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,23 @@ void	ft_div_mod(int a, int b, int *div, int *mod)
 {
 	*div = a / b;
 	*mod = a % b;
+}
+
+void	print_char(char c)
+{
+	write(1, &c, 1);	
+}
+
+void	main(void)
+{
+	int	a;
+	int	b;
+	int	div;
+	int	mod;
+
+	a = 17;
+	b = 5;
+	ft_div_mod(a, b, &div, &mod);
+	print_char(div + '0');
+	print_char(mod + '0');
 }
