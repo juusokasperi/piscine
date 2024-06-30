7a91c5288d269749dedefd85cb1def8616e8fb7c
juusokasperi <juusor@gmail.com>
2024-06-30 13:45:34 +0300

    started on c03

--- c03/ex01/ft_strncmp.c
+++ c03/ex01/ft_strncmp.c
@@ -1,24 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jrinta- <jrinta-@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/30 12:56:07 by jrinta-           #+#    #+#             */
-/*   Updated: 2024/06/30 13:30:57 by jrinta-          ###   ########.fr       */
+/*   Created: 2024/06/30 13:31:18 by jrinta-           #+#    #+#             */
+/*   Updated: 2024/06/30 13:44:31 by jrinta-          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int	ft_strcmp(char *s1, char *s2)
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	int	i;
+	unsigned int	i;
 
 	i = 0;
+	if (n < 1)
+		return (0);
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
+		if (i == n - 1)
+			return (s1[i] - s2[i]);
 		if (s1[i] != s2[i])
 			return (s1[i] - s2[i]);
 		i++;
--- c03main/ex01/ft_strncmp.c
+++ c03main/ex01/ft_strncmp.c
@@ -1,27 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jrinta- <jrinta-@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/30 12:56:07 by jrinta-           #+#    #+#             */
-/*   Updated: 2024/06/30 13:30:57 by jrinta-          ###   ########.fr       */
+/*   Created: 2024/06/30 13:31:18 by jrinta-           #+#    #+#             */
+/*   Updated: 2024/06/30 13:45:05 by jrinta-          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
 
-int	ft_strcmp(char *s1, char *s2)
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	int	i;
+	unsigned int	i;
 
 	i = 0;
+	if (n < 1)
+		return (0);
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
+		if (i == n - 1)
+			return (s1[i] - s2[i]);
 		if (s1[i] != s2[i])
 			return (s1[i] - s2[i]);
 		i++;
 	}
 	return (s1[i] - s2[i]);
+}
+
+int	main(void)
+{
+	char	str1[] = "testi";
+	char	str2[] = "testy";
+	int		i;
+
+	i = ft_strncmp(str1, str2, 20);
+	printf("%d\n", i);
+	return (0);
 }
